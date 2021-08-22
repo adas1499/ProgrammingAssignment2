e50d15133f0dc7f9b85e13806610b8bfd1c30e54
adas1499 <85026843+adas1499@users.noreply.github.com>
2021-08-23 02:01:04 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,22 @@
+## makeCacheMatrix creates an object for manipulating objects in cache
+## cacheSolve calculates the inverse of a matrix and records it in cache
+
+## This function defines four functions for storing the matrix and its inverse 
+## to cache
 makeCacheMatrix <- function(x = matrix()) {
   j <- NULL
   set <- function(y){
     x <<- y
     j <<- NULL
   }
+  # This function will retrieve x from the parent environment
   get <- function()x
   setInverse <- function(inverse) j <<- inverse
   getInverse <- function() j 
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
-
+## Calculates the inverse for a makeCacheMatrix object
+## Requires an object of class makeCacheMatrix as input
 cacheSolve <- function(x, ...) {
   j <- x$getInverse()
   if(!is.null(j)){
